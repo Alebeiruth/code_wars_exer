e9da2e36d29b47a333e0c54a89b077e7e63a39f8
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2023-11-24 17:35:16 +0000

    firts commit

---
+++ regexExpression.py
@@ -0,0 +1,9 @@
+import re ## permite uso de expressoes regulares em PY
+
+def validate_usr(username):
+    #your code here
+    return bool(re.fullmatch(r'^[a-z0-9_]{4,16}$', username))
+##re.fullmatch > verifica se str é uma expressão regular
+## ela busca expreção q começa ^ e termina em $, com 4 a 16 caracteres que podem ser de a-z,
+## numeros 0-9 ou sublinhados _
+## retorna True ou False
